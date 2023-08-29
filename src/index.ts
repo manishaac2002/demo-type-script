9d65056bdbb4d8e0ed94d9b700629745eec3cffc
manishaac2002 <manishaac2002@gmail.com>
2023-08-29 10:00:19 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let age :number =20;
+if (age < 50) 
+age+=10
+console.log(age);
+ 
+let level ;
+level =1;
+level ='a'
+
+console.log(level);
+
+function render(document) {
+ console.log(document);
+}
